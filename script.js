b7dbd9e78c19f71d02297b786a030f6ae7cf2c61
Nelson <nelson.ade.quadri@gmail.com>
2024-12-11 16:26:51 +0100

    Added script file, scroll to top button and form submission function

---
+++ script.js
@@ -0,0 +1,41 @@
+// get the button
+const scrollToTop = document.getElementById("scrollToTop");
+// Shoe when the scroll is up to a pasrticular height
+
+window.addEventListener("scroll", () => {
+//   console.log("here", window.scrollY);
+
+  if (window.scrollY > 700) {
+    scrollToTop.style.display = "block";
+  } else {
+    scrollToTop.style.display = "none";
+  }
+});
+// Scroll to the top, onClick of the button
+ 
+scrollToTop.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+})
+
+
+//  Form Sunbmision
+// get the form
+
+const contactForm = document.getElementById("contactForm");
+
+// add the eventlistener for the fom submission
+
+contactForm.addEventListener("submit", (event)=> {
+    event.preventDefault()
+
+    //show a success when clickeed on
+    Swal.fire({
+        title: "Submission Successful!",
+        text: "Thank You for submitting!",
+        icon: "success"
+      });
+})
+
